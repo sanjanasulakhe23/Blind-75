54dd92678336bbb002cc3dbb1fe4b836b7a2792e
Sanjana sulakhe <113519154+sanjanasulakhe23@users.noreply.github.com>
2025-10-26 00:18:12 +0530

    Time: 8 ms (47.41%) | Memory: 13.5 MB (81.43%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        left = 0
+        right = 1
+        
+        while right < len(nums):
+            if nums[left] == 0 and nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            elif nums[left] != 0:
+                left += 1
+            right += 1
